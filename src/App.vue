45463c83c99c3b4e13fbbaf1951fb52f8bef9502
ZRMYDYCG <547471919@qq.com>
2024-07-28 08:38:52 +0800

    feat: SearchView组件封装

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,7 @@ import { ref, reactive } from "vue"
     /*字体抗锯齿, 使得字体看起来更加清晰*/
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    font-size: 14px;
 }
 </style>
